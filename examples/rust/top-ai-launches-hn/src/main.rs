ee2a27938fe1e80340bead1f3254859994a596b9
Karthic Rao <kartronics85@gmail.com>
2023-10-05 14:58:10 +0800

    Add environment variable check and Docker detection
    - Check for the presence of the OPEN_AI_KEY environment variable.
    - Detect if the program is running inside a Docker container.
    - Provide user-friendly error messages with instructions on how to set the environment variable based on the runtime context (Docker vs. direct execution).
    
    This enhancement ensures that users are informed about the necessary environment configuration regardless of their execution method.

--- examples/rust/top-ai-launches-hn/src/main.rs
+++ examples/rust/top-ai-launches-hn/src/main.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
-use anyhow;
+use std::env;
+use std::path::Path;
+use anyhow::anyhow;
 use futures::stream::{self, StreamExt, TryStreamExt};
 use reqwest;
 use serde::{Deserialize, Serialize};
@@ -44,9 +46,30 @@ async fn handle_fetch_hn(_node_will_exec: NodeWillExecuteOnBranch) -> anyhow::Re
     Ok(serde_json::to_value(result).unwrap())
 }
 
+// check if we're running inside a docker container.
+// used to print meaningful error messages when running inside a container.
+fn is_running_inside_docker() -> bool {
+    Path::new("/.dockerenv").exists()
+}
+
 /// Maintain a list summarizing recent AI launches across the week
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+      // Check for the presence of the environment variable
+      // return gracefully with a meaningful error message if it's not set.
+      if env::var("OPEN_AI_KEY").is_err() {
+        if is_running_inside_docker() {
+            eprintln!("Error: OPEN_AI_KEY is not set!");
+            eprintln!("If you're running this from container, please set the environment variable using:");
+            eprintln!("\ndocker run -e OPEN_AI_KEY=your_key_here ...\n");
+        } else {
+            eprintln!("Error: OPEN_AI_KEY is not set!");
+            eprintln!("Please set the environment variable using:");
+            eprintln!("\nexport OPEN_AI_KEY=your_key_here\n");
+        }
+        return Err(anyhow!("Environment variable not set"));
+    }
+
     let mut c = Chidori::new(String::from("0"), String::from("http://localhost:9800"));
     c.start_server(Some(":memory:".to_string())).await?;
 
