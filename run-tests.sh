44d3bef406058733cb8e67bd1603937ad8bf3c0f
Karthic Rao <kartronics85@gmail.com>
2023-10-27 20:24:39 +0800

    script to run tests and teardown.

---
+++ run-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null
+then
+    echo "Docker is not installed. Please install Docker and try again."
+    exit 1
+fi
+
+# Check if Docker Compose is installed
+if ! command -v docker-compose &> /dev/null
+then
+    echo "Docker Compose is not installed. Please install Docker Compose and try again."
+    exit 1
+fi
+
+# Start the Docker Compose services and run the tests
+docker-compose up --exit-code-from tests
+
+# Tear down the Docker Compose services
+docker-compose down
